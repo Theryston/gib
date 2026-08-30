d6ecd44b9305c0534092b4d5874580f3e3f2b219
Theryston Santos <devtheryston@gmail.com>
2026-08-30 12:32:33 -0300

    Commit message: **Refactor project into workspace with split SDK and CLI crates**

--- src/commands/storage/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod add;
-mod list;
-mod prune;
-mod remove;
-
-pub use add::add;
-pub use list::list;
-pub use prune::prune;
-pub use remove::remove;
--- src/config/mod.rs
+++
@@ -1,9 +0,0 @@
-mod local;
-mod resolve;
-
-pub(crate) use local::{load_local_config, load_local_config_for_root};
-pub(crate) use resolve::{
-    PasswordPolicy, RepositoryOptions, load_and_report_local_config,
-    load_and_report_local_config_for_root, merge_ignore_patterns, resolve_path, resolve_repository,
-    resolve_repository_values,
-};
